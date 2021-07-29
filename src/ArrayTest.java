8941be077a59e8d301c22ff1040432ffc56db90b
Billccx <52842460+Billccx@users.noreply.github.com>
2021-07-29 14:12:36 +0800

    first commit

---
+++ src/ArrayTest.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] a=new int[100];
+        boolean[] b=new boolean[100];
+        String[] c=new String[100];
+        double [][] balences=new double[10][10];
+        //System.out.println(Arrays.deepToString(balences));
+        for(int i:a){
+            System.out.println(i);
+        }
+    }
+}
